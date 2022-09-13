cb30362a97c78e4fc10ade2b4de4165be05dfdb4
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.com>
2022-09-13 22:51:39 +0530

    eslint

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,36 @@
+/* eslint-disable react/destructuring-assignment */
 const WP_API = process.env.WP_URL;
 export default function Home(props) {
-  console.log(props);
-  const val = props.data.data.posts.nodes;
-  
-  return (
-    <>
-    {
-      val.length >0 ?
-          val.map((data)=>{
-            return(
-              <div key={data.date}>
-              <h1>
-                date:{data.date}
-              </h1>
-              <h1>
-                Slug:{data.slug}
-              </h1>
-              <h1>hello</h1>
-              </div>
+    // console.log(props);
+    const val = props.data.data.posts.nodes;
 
-
-            )
-          })
-      :<h1>no Posts found</h1>
-    }
-    </>
-  )
+    return (
+        <div>
+            {val.length > 0 ? (
+                val.map((data) => {
+                    return (
+                        <div key={data.date}>
+                            <h1>date:{data.date}</h1>
+                            <h1>Slug:{data.slug}</h1>
+                            <h1>hello</h1>
+                        </div>
+                    );
+                })
+            ) : (
+                <h1>no Posts found</h1>
+            )}
+        </div>
+    );
 }
 
-export async function getStaticProps(){
-    const res = await fetch(WP_API,{
-      method: "POST",
-      headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            query:`query NewQuery {
+export async function getStaticProps() {
+    const res = await fetch(WP_API, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `query NewQuery {
               posts {
                 nodes {
                   date
@@ -43,14 +38,13 @@ export async function getStaticProps(){
                   slug
                 }
               }
-            }`
-          })
-    })
+            }`,
+        }),
+    });
     const data = await res.json();
     console.log(data);
-  
+
     return {
-      props:{data}
-    }
-  
-  }
+        props: { data },
+    };
+}
